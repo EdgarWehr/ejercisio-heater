5315e405c6d9a091ef53acefd501ddeaf5d8b03a
Edgar Alberto Flores Wehr <wehredgar@gmail.com>
2019-08-26 11:00:03 -0500

    Intercambio inicial de proyecto

---
+++ Heater.java
@@ -0,0 +1,47 @@
+public class Heater
+{
+    private double temperature;
+    private double min;
+    private double max;
+    private double increment;
+    
+    public Heater()
+    {
+        temperature =15.0;
+        increment = 5.0;
+        min = 0;
+        max = 30;
+    }
+    
+    public Heater(double minValue, double maxValue)
+    {
+        temperature = 15.0;
+        increment = 5.0;
+        min = minValue;
+        max = maxValue;
+    }
+    
+    public double getTemperature()
+    {
+        return temperature;
+    }
+    
+    public void warmer()
+    {
+        if(temperature + increment < max){
+        temperature = temperature + increment;
+        }
+    }
+    
+    public void cooler()
+    {
+        if(temperature - increment > min){
+        temperature = temperature - increment;
+        }
+    }
+    
+    public void setIncrement(double newIncrement)
+    {
+        increment = newIncrement;
+    }
+}
